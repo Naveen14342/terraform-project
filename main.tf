3a77b413c21d23733d9b3f5e478c4c8746d8b2aa
Naveen14342 <naveentharapatla@gmail.com>
2025-11-30 15:06:55 +0530

    Fix resource reference for autoscaling attachment

--- main.tf
+++ main.tf
@@ -125,7 +125,7 @@ resource "aws_lb_listener" "http_listener" {
 
 resource "aws_autoscaling_attachment" "asg_attachment" {
   autoscaling_group_name = aws_autoscaling_group.asg.id
-  alb_target_group_arn   = aws_lb_target_group.app_tg.arn
+  lb_target_group_arn   = aws_lb_target_group.app_tg.arn
 }
 
 resource "aws_autoscaling_group" "asg" {
