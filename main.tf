3e55a07c6b0b316a07c9313d903fdda716262b24
Naveen14342 <naveentharapatla@gmail.com>
2025-11-30 15:01:00 +0530

    Add AWS Load Balancer resources to main.tf

--- main.tf
+++ main.tf
@@ -95,6 +95,39 @@ resource "aws_lb" "app_lb" {
   internal = false
 }
 
+resource "aws_lb_target_group" "app_tg" {
+  name     = "app-target-group"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb_listener" "http_listener" {
+  load_balancer_arn = aws_lb.app_lb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app_tg.arn
+  }
+}
+
+resource "aws_autoscaling_attachment" "asg_attachment" {
+  autoscaling_group_name = aws_autoscaling_group.asg.id
+  alb_target_group_arn   = aws_lb_target_group.app_tg.arn
+}
+
 resource "aws_autoscaling_group" "asg" {
    desired_capacity     = 2
    max_size             = 4
